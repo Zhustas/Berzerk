00ce59076602df0f2b05163ccd8bf08fcc5c0327
Justas <110050443+Zhustas@users.noreply.github.com>
2023-11-25 19:28:45 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/UnitTests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Program system project/Berzerk/UnitTests/lib
-# Build directory: D:/Program system project/Berzerk/cmake-build-debug/UnitTests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/UnitTests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Program system project/Berzerk/UnitTests/lib/googletest
-# Build directory: D:/Program system project/Berzerk/cmake-build-debug/UnitTests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
